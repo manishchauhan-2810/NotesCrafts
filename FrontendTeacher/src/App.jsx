f43220d000ff3273403db7d3fa7e28d320f02369
Manish chauhan <manish6380573@gmail.com>
2025-10-05 11:37:54 +0530

    Fixed merge conflicts and updated frontend code

--- FrontendTeacher/src/App.jsx
+++ FrontendTeacher/src/App.jsx
@@ -1,40 +1,3 @@
-<<<<<<< HEAD
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
-
-export default App
-=======
 import React, { useState } from 'react';
 import Dashboard from './Pages/Dashboard';
 import ClassDetail from './Pages/ClassDetail';
@@ -63,4 +26,3 @@ function App() {
 }
 
 export default App;
->>>>>>> f93394e62ae362567e4e0007f38ca62c7e5dfc78
--- FrontendTeacher/src/main.jsx
+++ FrontendTeacher/src/main.jsx
@@ -1,15 +1,3 @@
-<<<<<<< HEAD
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
-=======
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
@@ -17,8 +5,9 @@ import App from "./App";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
 );
->>>>>>> f93394e62ae362567e4e0007f38ca62c7e5dfc78
