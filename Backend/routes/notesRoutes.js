4dfe9129d2989efcd30d5e4b750654487b135cc4
Manish chauhan <manish6380573@gmail.com>
2025-10-05 11:01:20 +0530

    Save local changes before pull

--- Backend/routes/notesRoutes.js
+++ Backend/routes/notesRoutes.js
@@ -3,7 +3,6 @@ import { upload, uploadNote, getNotes } from "../controllers/notesController.js"
 
 const router = express.Router();
 
-// POST /api/notes/upload
 router.post("/upload", upload.single("file"), uploadNote);
 
 router.get("/", getNotes);
